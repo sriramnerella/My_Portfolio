e55669d3e7ea47c49dbc51e0ed0fa6b8d34e142c
Sriram <sriramnerella435@email.com>
2026-01-18 23:50:28 +0530

    Some changes

--- src/components/Dock.jsx
+++ src/components/Dock.jsx
@@ -3,8 +3,10 @@ import { dockApps } from '#constants';
 import { Tooltip } from 'react-tooltip';
 import { useGSAP } from '@gsap/react';
 import gsap from 'gsap';
+import useWindowStore from '#store/window';
 
 const Dock = () => {
+  const {openWindow , closeWindow , windows} = useWindowStore();
   const dockRef = useRef(null);
 
   useGSAP(()=>{
@@ -65,7 +67,27 @@ const Dock = () => {
 
 
 
-  const toggleApp = (app)=>{}
+  const toggleApp = (app)=>{
+
+    if(!app.canOpen) return;
+
+    const window = windows[app.id];
+
+    if(!window){
+      console.error(`No window configuration found for app id: ${app.id}`);
+      return;
+    }
+
+    if(window.isOpen){
+      closeWindow(app.id);
+    }
+    else{
+      openWindow(app.id);
+    }
+
+    console.log(windows);
+
+  }
   return <section id="dock">
     <div ref ={dockRef} className='dock-container'>
 
