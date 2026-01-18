fa7455b00a00f688f4456a5e9980447ff41fa998
Sriram <sriramnerella435@email.com>
2026-01-19 00:53:52 +0530

    implemented a higher order component and first window terminal

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,16 @@
 import {Navbar , Welcome , Dock } from '#components'
+import { Draggable } from 'gsap/Draggable'
+import gsap from 'gsap';
+import { Terminal } from '#windows';
+
+gsap.registerPlugin(Draggable);
 const App = () => {
   return (
    <main>
     <Navbar />
     <Welcome />
     <Dock />
+    <Terminal />
    </main>
   )
 }
